3fdf243123d6694b5119a633165670b8db54782f
Nareshbrabha <nirmalanitheesh15@gmail.com>
2024-11-14 14:29:21 +0530

    new files added

---
+++ summa.php
@@ -0,0 +1,16 @@
+ <div class="row justify-content-center">
+     <div class="col-md-6">
+         <h2 class="text-center">Student Login</h2>
+         <form action="" method="">
+             <div class="form-group">
+                 <label for="reg_no">Student_Reg_no</label>
+                 <input type="text" class="form-control" id="reg_no" name="reg_no" required>
+             </div>
+             <div class="form-group">
+                 <label for="password">Password</label>
+                 <input type="password" class="form-control" id="password" name="password" required autocomplete="current-password">
+             </div>
+             <button type="submit" class="btn btn-primary btn-block">Login</button>
+         </form>
+     </div>
+ </div>
